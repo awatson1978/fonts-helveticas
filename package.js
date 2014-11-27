994c4afa22703043ed7cd219d75f62c0fbd60fed
Abigail Watson <awatson1978@gmail.com>
2014-11-27 00:42:50 -0500

    updated add_files to addFiles

--- package.js
+++ package.js
@@ -6,27 +6,27 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.add_files('fonts/ColaborateLight.otf', 'client');
-    api.add_files('fonts/ColaborateBold.otf', 'client');
-    api.add_files('fonts/ColaborateMedium.otf', 'client');
-    api.add_files('fonts/ColaborateRegular.otf', 'client');
-    api.add_files('fonts/ColaborateThin.otf', 'client');
+    api.addFiles('fonts/ColaborateLight.otf', 'client');
+    api.addFiles('fonts/ColaborateBold.otf', 'client');
+    api.addFiles('fonts/ColaborateMedium.otf', 'client');
+    api.addFiles('fonts/ColaborateRegular.otf', 'client');
+    api.addFiles('fonts/ColaborateThin.otf', 'client');
 
-    api.add_files('fonts/ExistenceLight.otf', 'client');
-    api.add_files('fonts/ExistenceStencilLight.otf', 'client');
-    api.add_files('fonts/ExistenceUnicaseLight.otf', 'client');
+    api.addFiles('fonts/ExistenceLight.otf', 'client');
+    api.addFiles('fonts/ExistenceStencilLight.otf', 'client');
+    api.addFiles('fonts/ExistenceUnicaseLight.otf', 'client');
 
-    api.add_files('fonts/PrintClearly.otf', 'client');
-    api.add_files('fonts/PrintClearlyBold.otf', 'client');
-    api.add_files('fonts/PrintClearlyDashed.otf', 'client');
+    api.addFiles('fonts/PrintClearly.otf', 'client');
+    api.addFiles('fonts/PrintClearlyBold.otf', 'client');
+    api.addFiles('fonts/PrintClearlyDashed.otf', 'client');
 
-    api.add_files('fonts/QuicksandBold.otf', 'client');
-    api.add_files('fonts/QuicksandBoldOblique.otf', 'client');
-    api.add_files('fonts/QuicksandBook.otf', 'client');
-    api.add_files('fonts/QuicksandBookOblique.otf', 'client');
-    api.add_files('fonts/QuicksandDash.otf', 'client');
-    api.add_files('fonts/QuicksandLight.otf', 'client');
-    api.add_files('fonts/QuicksandLightOblique.otf', 'client');
+    api.addFiles('fonts/QuicksandBold.otf', 'client');
+    api.addFiles('fonts/QuicksandBoldOblique.otf', 'client');
+    api.addFiles('fonts/QuicksandBook.otf', 'client');
+    api.addFiles('fonts/QuicksandBookOblique.otf', 'client');
+    api.addFiles('fonts/QuicksandDash.otf', 'client');
+    api.addFiles('fonts/QuicksandLight.otf', 'client');
+    api.addFiles('fonts/QuicksandLightOblique.otf', 'client');
 
-    api.add_files('fonts-helveticas.css', 'client');
+    api.addFiles('fonts-helveticas.css', 'client');
 });
