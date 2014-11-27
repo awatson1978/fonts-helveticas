3ce5fe21d1a2dae4c9f70ce34d48889d044666f2
Abigail Watson <awatson1978@gmail.com>
2014-11-27 00:42:16 -0500

    updated documentation for meteor 1.0

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-**fonts-heveticas** is a Meteorite package for adding fonts in the Helvetica family to Meteor apps.   There are licensing restrictions around Helvetica, so this package relies on many pseudo-helvetica and helvetica-like fonts.  
+## fonts-helveticas  
 
-------------------------
-### Installation
+Meteorite package for adding fonts in the Helvetica family to Meteor apps.   There are licensing restrictions around Helvetica, so this package relies on many pseudo-helvetica and helvetica-like fonts.  
+
+==================================
+#### Installation
 
 First, install the fonts-helveticas package from the command line, like so:
 
 ````
-mrt add fonts-helveticas
+meteor add fonts-helveticas
 ````
 
-------------------------
-### Data/Document Model
+==================================
+#### Data/Document Model
 
-Second, add the 'helveticas' class to the appropriate HTML tags, like so.  Dead simple, right?
+Then, add the 'helveticas' class to the appropriate HTML tags, like so. 
 
 ````html
 <div>This is a barcode: <span class="helveticas">*123-45-6789*</span></div>
 ````
 
 
-------------------------
-### Syntax
+==================================
+#### Syntax
 
 fonts-helveticas provides the following CSS classes.  They're also available in camelCase, so feel free to use whichever casing works best in your syntax.
 
@@ -52,8 +54,8 @@ quicksand-light-oblique
 ````
 
 
-------------------------
-### Note
+==================================
+#### Note
 
 To be perfectly honest, the pseudo-helvetica fonts in this package are good, but not great.  They're workable substitutes for devices lacking a proper Helvetica Light font, but if anybody wants to swap them out for better selections, I'd be more than happy to accept pull requests.
 
--- package.js
+++ package.js
@@ -1,29 +1,32 @@
 Package.describe({
-  summary: "Meteorite package to add various fonts in the Helvetica family to a Meteor app."
+  summary: 'Meteorite package to add various fonts in the Helvetica family to a Meteor app.',
+  version: '1.0.0',
+  name: 'awatson1978:fonts-helveticas',
+  git: 'http://github.com/awatson1978/fonts-helveticas.git'
 });
 
 Package.on_use(function (api) {
-    api.add_files('fonts/ColaborateLight.otf', "client");
-    api.add_files('fonts/ColaborateBold.otf', "client");
-    api.add_files('fonts/ColaborateMedium.otf', "client");
-    api.add_files('fonts/ColaborateRegular.otf', "client");
-    api.add_files('fonts/ColaborateThin.otf', "client");
+    api.add_files('fonts/ColaborateLight.otf', 'client');
+    api.add_files('fonts/ColaborateBold.otf', 'client');
+    api.add_files('fonts/ColaborateMedium.otf', 'client');
+    api.add_files('fonts/ColaborateRegular.otf', 'client');
+    api.add_files('fonts/ColaborateThin.otf', 'client');
 
-    api.add_files('fonts/ExistenceLight.otf', "client");
-    api.add_files('fonts/ExistenceStencilLight.otf', "client");
-    api.add_files('fonts/ExistenceUnicaseLight.otf', "client");
+    api.add_files('fonts/ExistenceLight.otf', 'client');
+    api.add_files('fonts/ExistenceStencilLight.otf', 'client');
+    api.add_files('fonts/ExistenceUnicaseLight.otf', 'client');
 
-    api.add_files('fonts/PrintClearly.otf', "client");
-    api.add_files('fonts/PrintClearlyBold.otf', "client");
-    api.add_files('fonts/PrintClearlyDashed.otf', "client");
+    api.add_files('fonts/PrintClearly.otf', 'client');
+    api.add_files('fonts/PrintClearlyBold.otf', 'client');
+    api.add_files('fonts/PrintClearlyDashed.otf', 'client');
 
-    api.add_files('fonts/QuicksandBold.otf', "client");
-    api.add_files('fonts/QuicksandBoldOblique.otf', "client");
-    api.add_files('fonts/QuicksandBook.otf', "client");
-    api.add_files('fonts/QuicksandBookOblique.otf', "client");
-    api.add_files('fonts/QuicksandDash.otf', "client");
-    api.add_files('fonts/QuicksandLight.otf', "client");
-    api.add_files('fonts/QuicksandLightOblique.otf', "client");
+    api.add_files('fonts/QuicksandBold.otf', 'client');
+    api.add_files('fonts/QuicksandBoldOblique.otf', 'client');
+    api.add_files('fonts/QuicksandBook.otf', 'client');
+    api.add_files('fonts/QuicksandBookOblique.otf', 'client');
+    api.add_files('fonts/QuicksandDash.otf', 'client');
+    api.add_files('fonts/QuicksandLight.otf', 'client');
+    api.add_files('fonts/QuicksandLightOblique.otf', 'client');
 
-    api.add_files('fonts-helveticas.css', "client");
+    api.add_files('fonts-helveticas.css', 'client');
 });
